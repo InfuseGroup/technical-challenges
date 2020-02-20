aeedbf72691693fd439c564139617383f2c811c6
LiamBorner <liamborner@gmail.com>
2020-02-20 21:05:49 +0000

    working slider

--- app/javascript/components/challenges/Cards.tsx
+++ app/javascript/components/challenges/Cards.tsx
@@ -4,10 +4,9 @@ import Carousel from "./Carousel"
 const nasaApiKey = '6H6EdNLLrDu8SC1LZMJkbJzoGIghjvrjzgQpF72W';
 const baseUri = 'https://api.nasa.gov/planetary/apod';
 
-
 // not the most elegant date randomiser, I know, but works (sort of)
   var myDate = "2019-0"+Math.floor(Math.random()*12).toString() +"-" +  Math.floor(Math.random()*29).toString();
-   console.log(myDate);
+
 
 function getImage(date: string) {
   return fetch(`${baseUri}?api_key=${nasaApiKey}&date=${myDate}`)
@@ -19,9 +18,30 @@ function getImage(date: string) {
     });
 }
 
+console.log(myDate);
+
+
+const images = [
+{ src: "https://apod.nasa.gov/apod/image/2002/freeflyer_nasa_960.jpg" },
+{ src: "https://apod.nasa.gov/apod/image/2002/SolarOrbiterLaunch_Demeter_960.jpg" },
+{ src: "https://apod.nasa.gov/apod/image/2002/EclipseCamel_Cripps_960.jpg" },
+{ src: "https://apod.nasa.gov/apod/image/2001/C2017T2_2020-01-24-26-28_posta1024.jpg" }
+
+];
+
+// const [activeImage, setActiveImage] = React.useState(0);
+//
+// const randomImages = e => {
+// const len = images.length;
+// setActiveImage(Math.floor(Math.random() * len))
+//
+// };
+
+
 export default function Card({ date }) {
 
-  const [imageUrl, setImage] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const [image, setImage] = React.useState('');
 
   React.useEffect(() => {
     getImage(date).then(response => setImage(response));
@@ -42,19 +62,6 @@ export default function Card({ date }) {
   }, []);
 
 
-const childCards = ['2020-02-13', '2020-02-12', '2020-02-02', '2020-02-01'].map((date) => {
-    // return <Card date={date} />
-  });
-
-  const images = [
-{ src: "https://apod.nasa.gov/apod/image/2002/freeflyer_nasa_960.jpg" },
-{ src: "https://apod.nasa.gov/apod/image/2002/SolarOrbiterLaunch_Demeter_960.jpg" },
-{ src: "https://apod.nasa.gov/apod/image/2002/EclipseCamel_Cripps_960.jpg" },
-{ src: "https://apod.nasa.gov/apod/image/2002/freeflyer_nasa_960.jpg" }
-
-];
-
-
   const buttonStyles: React.CSSProperties = {
     padding: '10px 20px',
     background: 'grey',
@@ -71,30 +78,26 @@ const childCards = ['2020-02-13', '2020-02-12', '2020-02-02', '2020-02-01'].map(
 
       <h3>2. Convert the images into a slider using the pagination buttons.</h3>
 
-// styling to fix here as it's bleeding into the stuff below
-  <div className="app">
       <div className="cards">
-     <div className="wrapper">
-       <Carousel initialStep={1} images={images} />
-     </div>
-     </div>
-   </div>
+        <div className="wrapper">
+          <Carousel className="carousel" initialStep={1} images={images} />
+        </div>
+      </div>
 
 //image randomiser exists, but need to work out how to make onClick work
       <h3>Randomised Image</h3>
       <h3>1. Randomise the image when you click the button.</h3>
 
       <div className="cards">
-        { childCards }
-      </div>
-
-      <div className="cards">
-        <div className="card" style={{ backgroundImage: `url(${imageUrl})` }} />
+          <div className="card" style={{ backgroundImage: `${baseUri}?api_key=${nasaApiKey}&date=${myDate}` }} />
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button style={buttonStyles}  >Randomise</button>
+        <button style={buttonStyles} >Randomise</button>
 
       </div>
-    </div>
+
+
+      </div>
+
   );
 };
